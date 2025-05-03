f994a0542da1fac94df1f7edc74020f0efea689e
José Fernando Costa <jose.fernando.costa.1998@gmail.com>
2025-05-03 11:57:16 +0000

    Vite config changes for replit deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ export default defineConfig({
   server: {
     host: true, // Listen on all network interfaces
     port: 5173,
-    allowedHosts: ["6c9d6251-f5ce-4e7a-bb91-260e32324d97-00-3adfbrczjcatn.riker.replit.dev"]
+    allowedHosts: [
+      "6c9d6251-f5ce-4e7a-bb91-260e32324d97-00-3adfbrczjcatn.riker.replit.dev",
+      "markdown-notes-app-ze1598.replit.app",
+    ],
+  },
+  preview: {
+    allowedHosts: [
+      "6c9d6251-f5ce-4e7a-bb91-260e32324d97-00-3adfbrczjcatn.riker.replit.dev",
+      "markdown-notes-app-ze1598.replit.app",
+    ],
   },
 });
